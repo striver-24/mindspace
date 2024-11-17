04824148223694b632c3b80983e33612982cbdfc
Deivyansh Singh <106349960+striver-24@users.noreply.github.com>
2024-11-18 04:39:40 +0530

    Final Document List Commit

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -47,13 +47,14 @@ export const Item = ({
     const onCreate = (
         event: React.MouseEvent<HTMLDivElement, MouseEvent>
     ) => {
+        event.stopPropagation();
         if (!id) return;
         const promise = create({ title: "Untitled", parentDocument: id})
             .then((documentId) => {
                 if(!expanded) {
                     onExpand?.();
                 }
-                router.push(`/documents/${documentId}`);
+                //router.push(`/documents/${documentId}`);
             });
         toast.promise(promise, {
             loading: "Creating a new note...",
@@ -102,6 +103,8 @@ export const Item = ({
             {!!id && (
                 <div className="ml-auto flex items-center gap-x-2">
                     <div
+                        role = "button"
+                        onClick={onCreate}
                         className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
                     >
                         <Plus className="h-4 w-4 text-muted-foreground" />
